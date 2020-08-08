32c37c6866bbcc73e76b06c2a1932d7c81991f9a
steve97harris <steve97harris@hotmail.co.uk>
2020-08-08 18:54:17 +0100

    the beginning

---
+++ Assets/Scripts/DashBoardManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using UnityEngine;
+
+public class DashBoardManager : MonoBehaviour
+{
+    private GameObject dashboard = null;
+    private GameObject pointsPage = null;
+    private GameObject teamPage = null;
+    private GameObject transfersPage = null;
+
+    void Start()
+    {
+        dashboard = GameObjectFinder.FindSingleObjectByName("Dashboard");
+        pointsPage = GameObjectFinder.FindSingleObjectByName("PointsPage");
+        teamPage = GameObjectFinder.FindSingleObjectByName("TeamPage");
+        
+        dashboard.SetActive(true);
+        pointsPage.SetActive(false);
+        teamPage.SetActive(false);
+        transfersPage.SetActive(false);
+    }
+
+    public void PointsButton()
+    {
+        pointsPage.SetActive(true);
+        dashboard.SetActive(false);
+        teamPage.SetActive(false);
+        transfersPage.SetActive(false);
+    }
+
+    public void TeamButton()
+    {
+        teamPage.SetActive(true);
+        dashboard.SetActive(false);
+        pointsPage.SetActive(false);
+        transfersPage.SetActive(false);
+    }
+
+    public void TransfersButton()
+    {
+        transfersPage.SetActive(true);
+        dashboard.SetActive(false);
+        pointsPage.SetActive(false);
+        teamPage.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/GameObjectFinder.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class GameObjectFinder : MonoBehaviour
+    {
+        public static GameObject[] FindMultipleObjectsByName(string objectName)
+        {
+            var objects = Resources.FindObjectsOfTypeAll<GameObject>().Where(obj => obj.name == objectName).ToArray();
+            return objects;
+        }
+        
+        public static GameObject FindSingleObjectByName(string objectName)
+        {
+            var objects = Resources.FindObjectsOfTypeAll<GameObject>().Where(obj => obj.name == objectName).ToArray();
+            return objects[0];
+        }
+    }
+}
