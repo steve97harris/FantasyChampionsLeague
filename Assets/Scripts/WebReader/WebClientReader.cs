15298c8b117bd2052dfbbf89edafb1429b150cf7
steve97harris <34012174+steve97harris@users.noreply.github.com>
2020-09-17 16:30:43 +0100

    lkdjf

--- Assets/Scripts/WebReader/WebClientReader.cs
+++ Assets/Scripts/WebReader/WebClientReader.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Text.RegularExpressions;
 using DefaultNamespace;
 using HtmlAgilityPack;
@@ -23,56 +25,67 @@ namespace WebReader
         
         private const string MatchInfoList = "//ul[@class='info-list']";
         private const string MatchInfoStats = MatchInfoList + "/li[@class='matchInfoStats']";
-        private const string TeamInfo = MatchInfoList + "/li[@class]";
-        private const string Competitions = "//div[@class='heading-box']";
+        private const string InfoList = MatchInfoList + "/li[@class]";
+        private const string HeadingBox = "//div[@class='heading-box']";
+        private const string InfoBlockC323 = "//div[@class='info-block c_323']";
+        private const string CompSpan = "//span[@id]";
         
         #endregion
 
         private void Start()
         {
             GetWebText();
+            TeamsPlayingToday();
         }
 
         private void GetWebText()
         {
             var web = new HtmlWeb();
             var doc = web.Load(Url);
-            var teamInfoNodes = doc.DocumentNode.SelectNodes(TeamInfo);
-            var competitionNodes = doc.DocumentNode.SelectNodes(Competitions);
-            
-            var fixtures = new List<string>();
-            var goalsList = new List<string>();
+            var compSpanNodes = doc.DocumentNode.SelectNodes(CompSpan);
 
-            for (int i = 0; i < teamInfoNodes.Count - 1; i++)
+            var infoList = new List<string>();
+            for (int i = 0; i < compSpanNodes.Count; i++)
             {
-                var currNode = teamInfoNodes[i].InnerText;
-                currNode = RemoveWhitespace(currNode);
-
-                if (currNode.Contains("&nbsp;"))
+                if (!compSpanNodes[i].Id.Contains("compspan"))
+                    continue;
+                
+                var headerNode = compSpanNodes[i].SelectSingleNode("./div[@class='heading-box']");
+                var matchInfoCollection = compSpanNodes[i].SelectNodes("./following::ul/li");
+                
+                if (infoList.Count > matchInfoCollection.Count)
                 {
-                    goalsList.Add(currNode);
+                    var diff = infoList.Count - matchInfoCollection.Count;
+                    infoList.RemoveRange(diff,infoList.Count - diff);
                 }
-                else
+                
+                infoList.Add(RemoveWhitespace(headerNode.InnerText));
+                for (int j = 0; j < matchInfoCollection.Count; j++)
                 {
-                    currNode = currNode.Replace(",", " ");
-                    fixtures.Add(currNode);
+                    infoList.Add(RemoveWhitespace(matchInfoCollection[j].InnerText));
                 }
             }
+
+            var gamesWithoutTime = 0;
+            var fixtures = new List<string>();
+            var goalAssistList = new List<string>();
+            for (int i = 0; i < infoList.Count; i++)
+            {
+                if (infoList[i].StartsWith("-"))
+                    gamesWithoutTime++;
+                
+                if (infoList[i].Contains("&nbsp;"))
+                    goalAssistList.Add(infoList[i]);
+                else
+                    fixtures.Add(infoList[i]);
+            }
+            fixtures.RemoveRange(fixtures.Count - gamesWithoutTime, gamesWithoutTime);
 
             var goalScorers = new Dictionary<string, int>();
             var assists = new Dictionary<string, int>();
-            for (int i = 0; i < goalsList.Count; i++)
+            for (int i = 0; i < goalAssistList.Count; i++)
             {
-                ExtractNames(goalsList[i], goalScorers, assists);
-            }
-
-            for (int i = 0; i < competitionNodes.Count; i++)
-            {
-                var compNode = competitionNodes[i].InnerText;
-                compNode = RemoveWhitespace(compNode);
-                compNode = compNode.Replace(",", " ");
-                
-                Debug.LogError(compNode);
+                ExtractNames(goalAssistList[i], goalScorers, assists);
             }
             
             SetFixturesUi(fixtures);
@@ -80,14 +93,22 @@ namespace WebReader
         
         private void SetFixturesUi(List<string> fixtures)
         {
-            var fixtureTextObj = GameObjectFinder.FindSingleObjectByName("FixturesText");
-            var fixtureMerge = "";
+            var fixtureTemplate = Resources.Load<GameObject>("Prefabs/FixturesPanel/FixtureTemplate");
+            var fixtureContent = GameObjectFinder.FindSingleObjectByName("FixturesContent");
+            
+            foreach (Transform child in fixtureContent.transform)
+            {
+                Destroy(child.gameObject);
+            }
+            
             for (int i = 0; i < fixtures.Count; i++)
             {
-                fixtureMerge += fixtures[i] + Environment.NewLine;
+                fixtures[i] = fixtures[i].Replace(",", " ");
+                fixtures[i] = DiacriticsRemover.RemoveDiacritics(fixtures[i]);
+                
+                var fixtureTemp = Instantiate(fixtureTemplate, fixtureContent.transform);
+                fixtureTemp.GetComponentInChildren<TMP_Text>().text = fixtures[i];
             }
-
-            fixtureTextObj.GetComponent<TMP_Text>().text = fixtureMerge;
         }
 
         private void ExtractNames(string goalInfo, IDictionary<string, int> goalScorers, IDictionary<string, int> assists) 
@@ -139,6 +160,27 @@ namespace WebReader
             }
         }
         
+        private void TeamsPlayingToday()
+        {
+            var web = new HtmlWeb();
+            var doc = web.Load(Url);
+            var nodeCollection = doc.DocumentNode.SelectNodes("//span[@class='text']");
+            
+            for (int i = 0; i < nodeCollection.Count; i++)
+            {
+                var nodeString = nodeCollection[i].InnerText;
+                nodeString = RemoveWhitespace(nodeString);
+                nodeString = nodeString.Replace(",", " ");
+                nodeString = nodeString.Replace(".", "");
+                nodeString = DiacriticsRemover.RemoveDiacritics(nodeString);
+                
+                if (!IsTeamName(nodeString))
+                    continue;
+
+                Debug.LogError(nodeString);
+            }
+        }
+        
         private string RemoveWhitespace(string str)
         {
             var res = Regex.Replace(str, @"\s+", ",");
@@ -154,6 +196,13 @@ namespace WebReader
             var startIndexOfAssistBy = s.IndexOf("assistby", StringComparison.Ordinal);
             s = s.Remove(startIndexOfAssistBy, "assistby".Length);
             return s;
+        }
+
+        private bool IsTeamName(string str)
+        {
+            var isInt = int.TryParse(str, out var result);
+            var startWithInt = int.TryParse(str[0].ToString(), out var res);
+            return !isInt && !startWithInt && !string.IsNullOrWhiteSpace(str) && !str.Contains(':') && str != "-" && !str.Contains('(') && !str.Contains(')');
         }
     }
 }
